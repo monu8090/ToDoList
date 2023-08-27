12676ad46cf119f422fc12f6ba97eb956a19399b
Diby Prakash Maury <143277331+monu8090@users.noreply.github.com>
2023-08-27 21:08:18 +0530

    Add files via upload

---
+++ Todo.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Document</title>
+     <!----Responsive link---->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!---Font-awesome---->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/js/
+        fontawesome.min.js"></script>
+            <!--- Css Link--->
+             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
+integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" 
+crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+      <link href="Hotel.css" rel="stylesheet" type="text/css"/>
+      <link href="Todo.css" rel="stylesheet" type="text/css"/>
+    <!----Java Script------>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+         </head>
+
+
+
+
+<body>
+<div class="container">
+    <h1>To Do App</h1>
+    <div class="inputs">
+        <input type="text" placeholder="Enter your Task" id="input">
+        <button onclick="Add()">Add</button>
+    </div>
+    <div class="text">
+
+    </div>
+</div>
+
+<script src="Todo.js"></script>
+</body>
+</html>
